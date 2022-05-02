94c121d6feb19791f65065d34819d41088fd3dbf
Louis Schell <louis.schell1@gmx.de>
2022-05-02 18:19:36 +0200

    Add timing for enemy move and better error messaging for irregular game endings. Formatted

--- client/src/client.rs
+++ client/src/client.rs
@@ -1,12 +1,12 @@
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
 
 use log::info;
-
 use socha_client_2022::client::{DebugMode, SCClient, SCClientDelegate};
 use socha_client_2022::game::Move as SCMove;
 use socha_client_2022::game::State as SCState;
 use socha_client_2022::game::Team as SCTeam;
 use socha_client_2022::protocol::GameResult;
+use socha_client_2022::protocol::ScoreCause;
 use socha_client_2022::util::SCResult;
 
 use crate::algorithms::Algorithm;
@@ -14,6 +14,8 @@ use crate::game::{Fen, Gamestate, Team};
 
 struct ClientDelegate<E: Algorithm> {
     inner: E,
+    client_team: Option<Team>,
+    time_tracker: Option<SystemTime>,
 }
 
 impl<E: Algorithm> SCClientDelegate for ClientDelegate<E> {
@@ -21,6 +23,19 @@ impl<E: Algorithm> SCClientDelegate for ClientDelegate<E> {
         let gamestate: Gamestate = state.clone().into();
         println!("{}", gamestate.to_fen());
         println!("{}", gamestate.board);
+
+        if let Some(player) = self.client_team {
+            if let Some(time) = self.time_tracker {
+                if gamestate.current_player == player {
+                    info!(
+                        "Enemy took: {:?}",
+                        time.elapsed()
+                            .unwrap()
+                            .saturating_sub(Duration::from_millis(17)) //heuristic value
+                    );
+                }
+            }
+        }
     }
 
     fn on_game_end(&mut self, result: &GameResult, my_team: SCTeam) {
@@ -46,9 +61,9 @@ impl<E: Algorithm> SCClientDelegate for ClientDelegate<E> {
                 }
             })
             .unwrap();
-        let my_score = match my_team {
-            SCTeam::One => red_score,
-            SCTeam::Two => blue_score,
+        let (my_score, enemy_score) = match my_team {
+            SCTeam::One => (red_score, blue_score),
+            SCTeam::Two => (blue_score, red_score),
         };
         if let Some(winner) = result.winner() {
             if my_team == winner.team() {
@@ -58,7 +73,7 @@ impl<E: Algorithm> SCClientDelegate for ClientDelegate<E> {
                     blue_score.parts().get(1).unwrap(),
                     red_score.parts().get(0).unwrap(),
                     blue_score.parts().get(0).unwrap(),
-                    my_score.cause()
+                    enemy_score.cause()
                 );
             } else {
                 info!(
@@ -71,28 +86,47 @@ impl<E: Algorithm> SCClientDelegate for ClientDelegate<E> {
                 );
             }
         } else {
-            info!(
-                "DRAW({}) [{:?}]",
-                red_score.parts().get(0).unwrap(),
-                my_score.cause()
-            );
+            if !matches!(my_score.cause(), ScoreCause::Regular) {
+                info!(
+                    "SCORE({} : {})  [{:?}]",
+                    red_score.parts().get(1).unwrap(),
+                    blue_score.parts().get(1).unwrap(),
+                    my_score.cause()
+                );
+            } else if !matches!(enemy_score.cause(), ScoreCause::Regular) {
+                info!(
+                    "SCORE({} : {})  [{:?}]",
+                    red_score.parts().get(1).unwrap(),
+                    blue_score.parts().get(1).unwrap(),
+                    enemy_score.cause()
+                );
+            } else {
+                info!(
+                    "DRAW({}) [{:?}]",
+                    red_score.parts().get(0).unwrap(),
+                    my_score.cause()
+                );
+            }
         }
     }
 
     fn on_welcome(&mut self, team: SCTeam) {
         let team: Team = team.into();
         info!("Joined as Team {}", team);
+        self.client_team = Some(team);
     }
 
     fn request_move(&mut self, sc_state: &SCState, my_team: SCTeam) -> SCMove {
         info!("Beginning move calculation");
-        let start = SystemTime::now();
-
+        self.time_tracker = Some(SystemTime::now());
         let best = self
             .inner
             .best_move(sc_state.clone().into(), my_team.into());
-        let stop = SystemTime::now().duration_since(start).unwrap();
-        info!("Finished calculation {:?}", stop);
+        info!(
+            "Finished calculation {:?}",
+            self.time_tracker.unwrap().elapsed().unwrap()
+        );
+        self.time_tracker = Some(SystemTime::now());
         let piece = best.piece;
         let mov = best.into();
         info!("Sending move: {} {}", piece, mov);
@@ -106,7 +140,11 @@ pub struct Client<A: Algorithm> {
 
 impl<A: Algorithm> Client<A> {
     pub fn new(algorithm: A, reservation_code: Option<String>) -> Self {
-        let algorithm_wrapper = ClientDelegate { inner: algorithm };
+        let algorithm_wrapper = ClientDelegate {
+            inner: algorithm,
+            client_team: None,
+            time_tracker: None,
+        };
         Self {
             inner: SCClient::new(
                 algorithm_wrapper,
