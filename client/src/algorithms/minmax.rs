74419e49c853d7a391a75ee9387dec03e2d4445a
j0bIT <jonaspp471@gmail.com>
2022-05-27 15:46:52 +0200

    iterative deepening not well-thought-out II

--- client/src/algorithms/minmax.rs
+++ client/src/algorithms/minmax.rs
@@ -1,3 +1,4 @@
+use std::borrow::BorrowMut;
 use crate::algorithms::{Algorithm, EvaluationFunction};
 use crate::game::{Gamestate, IGamestate, Move, Team};
 use num_traits::{Bounded, Num, NumCast, Zero};
@@ -54,14 +55,9 @@ impl<E: MinMaxState> MinMaxNode<E> {
         self.children.sort_by_key(|child| child.evaluation);
     }
 
-    fn sort_tree(&mut self) {
-        if self.is_leaf() {
-            return;
-        }
-
-        for child in &mut self.children {
-            child.sort_children();
-            child.sort_tree();
+    fn pretty_print(&self) {
+        for child in self.children.iter() {
+            println!("value: {}", child.evaluation);
         }
     }
 }
@@ -82,79 +78,96 @@ impl<E: MinMaxState> MinMaxTree<E> {
             evaluation_function,
             my_team,
             start_timer,
-            max_search_duration
+            max_search_duration,
         }
     }
 
     pub fn iterate(&mut self, depth: u8) -> E::MoveType {
-        self.root.sort_tree();
+        self.root.pretty_print();
+        println!("------------------");
+        self.root.sort_children();
 
-        let mut move_value_pairs = vec![];
 
         if self.root.is_leaf() {
             self.root.expand(self.my_team, true);
         }
+
+        let mut move_value_index_pairs = vec![];
         //Maximizing player (Client player)
-        for child_node in self.root.children.iter_mut() {
-            let eval = Self::min_max(child_node, depth, self.my_team, E::EvalType::min_value(), E::EvalType::max_value(), self.start_timer, self.max_search_duration, self.my_team, self.evaluation_function);
-            move_value_pairs.push((eval, child_node.mov.unwrap()));
+        for (index, child_node) in self.root.children.iter().enumerate() {
+            let eval = self.min_max(child_node.state, depth, self.my_team.opponent(), E::EvalType::min_value(), E::EvalType::max_value());
+            move_value_index_pairs.push((eval, child_node.mov.unwrap(), index));
         }
 
-        let max = move_value_pairs.iter().max_by_key(|pair| pair.0);
+        for (value, _, index) in move_value_index_pairs.iter() {
+            match self.root.children.get_mut(*index) {
+                Some(child) => child.evaluation = *value,
+                _ => {}
+            }
+        }
+
+        let max = move_value_index_pairs.iter().max_by_key(|pair| pair.0);
         println!("Best Evaluation: {}", max.unwrap().0);
         println!("Depth {}", depth);
         max.unwrap().1.clone()
     }
 
-    fn min_max(
-        node: &mut MinMaxNode<E>,
-        depth: u8,
-        team: Team,
-        mut alpha: E::EvalType,
-        mut beta: E::EvalType,
-        start_timer: Instant,
-        max_search_duration: Duration,
-        my_team: Team,
-        evaluation_function: EvaluationFunction<E, E::EvalType>,
+    fn min_max(&self,
+               state: E,
+               depth: u8,
+               team: Team,
+               mut alpha: E::EvalType,
+               mut beta: E::EvalType,
     ) -> E::EvalType {
-
-        if depth == 0 || node.state.game_over() || start_timer.elapsed().as_millis() > max_search_duration.as_millis() {
-            let eval = (evaluation_function)(&node.state, my_team);
-            node.evaluation = eval;
+        if depth == 0 || state.game_over() {
+            let eval = (self.evaluation_function)(&state, self.my_team);
+            //println!("{}", eval);
             return eval;
         }
+        let is_maximizing = team == self.my_team;
 
-        if node.is_leaf() {
-            node.expand(team, false);
+        if self.start_timer.elapsed().as_millis() > self.max_search_duration.as_millis() {
+            return if is_maximizing {
+                alpha
+            } else {
+                beta
+            }
         }
 
-        let is_maximizing = team == my_team;
 
 
-        return if is_maximizing {
+        if is_maximizing {
             //Maximizing player (Client player)
             let mut max_eval = E::EvalType::max_value();
 
-            for child_node in node.children.iter_mut() {
-                let eval = Self::min_max(child_node, depth - 1, team.opponent(), alpha, beta, start_timer, max_search_duration, my_team, evaluation_function);
+            state.for_each_move(team, &mut |mov| {
+                let mut child_state = state.clone();
+                child_state.apply_move(&mov);
+                child_state.next_player();
+
+                let eval = self.min_max(child_state, depth - 1, team.opponent(), alpha, beta);
                 max_eval = E::EvalType::max(max_eval, eval);
                 alpha = E::EvalType::max(alpha, eval);
 
-                if beta <= alpha { break };
-            }
+                if beta <= alpha { return; }
+            });
 
-            max_eval
+            return max_eval;
         } else {
             //Minimizing player (Client player)
             let mut min_eval = E::EvalType::min_value();
 
-            for child_node in node.children.iter_mut() {
-                let eval = Self::min_max(child_node, depth - 1, team.opponent(), alpha, beta, start_timer, max_search_duration, my_team, evaluation_function);
-                min_eval = E::EvalType::min(min_eval, eval);
-                beta = E::EvalType::min(beta, eval); // TODO alpha beta?
+            state.for_each_move(team, &mut |mov| {
+                let mut child_state = state.clone();
+                child_state.apply_move(&mov);
+                child_state.next_player();
 
-                if beta <= alpha { break };
-            }
+                let eval = self.min_max(child_state, depth - 1, team.opponent(), alpha, beta);
+                min_eval = E::EvalType::min(min_eval, eval);
+                beta = E::EvalType::min(beta, eval);
+
+                if beta <= alpha { return; }
+            });
 
             min_eval
         }
@@ -177,7 +190,6 @@ impl<E: MinMaxState> MinMax<E> {
         state: E,
         my_team: Team,
     ) -> E::MoveType {
-
         let start_timer = Instant::now();
 
         let mut depth = 2;
